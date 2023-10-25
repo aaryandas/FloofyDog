9f6c91077fdf926e3411365797d41d9e37bc167d
Jonathan Hop <yithril@gmail.com>
2023-10-25 10:35:05 -0400

    review

--- AccountingLedgerCapstone/src/main/java/org/example/Main.java
+++ AccountingLedgerCapstone/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        //That's cool, I'm putting this so there is a change to trigger a pull request
         System.out.println("Still Working on Program");
 
     }
