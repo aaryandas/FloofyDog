64ec2cebdf09f0ce67ddd47d843868296ad6e75c
Jonathan Hop <yithril@gmail.com>
2023-11-08 12:11:55 -0500

    comments

--- WeeklyProjects/CarDealership/src/main/java/org/example/Main.java
+++ WeeklyProjects/CarDealership/src/main/java/org/example/Main.java
@@ -1,5 +1,8 @@
 package org.example;
 
+//Overall a solid job, you get separation of concerns
+//Each class sticks to its responsibility without overlap
+//No need for in depth comments, it's good work.
 public class Main {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
